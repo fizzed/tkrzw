9aac5c8b22109a78133e7be570648b47a532fe5c
Joe Lauer <joe@lauer.bz>
2023-01-05 14:11:54 -0500

    Add windows arm64!

--- setup/build-native-lib-windows-action.bat
+++ setup/build-native-lib-windows-action.bat
@@ -5,19 +5,36 @@ set BASEDIR=%~dp0
 cd %BASEDIR%\..
 set PROJECTDIR=%CD%
 
+set BUILDOS=%1
+set BUILDARCH=%2
+
+if "%BUILDOS%"=="" (
+  echo Usage: script [buildos] [buildarch]
+  exit /B
+)
+
+if "%BUILDARCH%"=="" (
+  echo Usage: script [buildos] [buildarch]
+  exit /B
+)
+
 echo
 echo Project Dir: %PROJECTDIR%
 echo
 
+set VCVARSALLARCH=x64
+if "%BUILDARCH%"=="arm64" (
+  set VCVARSALLARCH=x64_arm64
+)
+
 @REM can we install visual studio variables?
-@call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_arm64 %*
+@call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" %VCVARSALLARCH% %*
 
 @REM we need to swap in VCToolsInstallDir (but it has a trailing slash)
 set VCTOOLSDIR=%VCToolsInstallDir:~0,-1%
 
 mkdir target
 rsync -avrt --delete ./native/ ./target/
-mkdir target\output
 
 set
 
@@ -38,7 +55,7 @@ set SDKPATH=%SDKPATH:~0,-1%
 @REM https://stackoverflow.com/questions/44262083/which-sdk-do-i-need-to-ensure-windows-7-compatibility-in-visual-studio-c-2017
 @REM 0x0601 will be windows 7 and up
 @REM 0x0A00 will be windows 10 and up
-set WINDOWS_VERSION_TARGET=0x0A00
+set WINDOWS_VERSION_TARGET=0x0601
 
 @REM for sed to work with a path being replaced, we need to escape the \ chars twice
 set VCPATH_ESCAPED=%VCPATH:\=\\\%
@@ -68,4 +85,4 @@ sed -i -e 's/x64/%TGTARCH%/' VCMakefile
 nmake -f VCMakefile
 nmake -f VCMakefile check
 
-copy .\jtkrzw.dll ..\output\
+copy .\jtkrzw.dll ..\..\tkrzw-%BUILDOS%-%BUILDARCH%\src\main\resources\jne\%BUILDOS%\%BUILDARCH%\
