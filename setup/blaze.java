933ffdae4d2ec72bedaf873b96b1c20edcccd163
Joe Lauer <joe@lauer.bz>
2023-01-07 01:55:44 -0500

    Fully working build system

--- setup/blaze.java
+++ setup/blaze.java
@@ -13,92 +13,112 @@ import static com.fizzed.blaze.Systems.exec;
 import com.fizzed.blaze.ssh.SshSession;
 import static com.fizzed.blaze.SecureShells.sshConnect;
 import static com.fizzed.blaze.SecureShells.sshExec;
-import com.fizzed.blaze.buildx.*;
+import com.fizzed.buildx.*;
 
-public class blaze extends BlazeBuildx {
+public class blaze {
 
-    @Override
-    protected List<Target> targets() {
-        return asList(
-            // Windows x64 (win7+)
-            new Target("windows", "x64", "bmh-build-x64-win11-1", null),
+    private final List<Target> targets = asList(
+        // Windows x64 (win7+)
+        new Target("windows", "x64")
+                .setTags("build", "test")
+                .setHost("bmh-build-x64-win11-1"),
 
-            // Windows arm64 (win10+)
-            new Target("windows", "arm64", "bmh-build-x64-win11-1", null),
+        // Windows arm64 (ONLY for building)
+        new Target("windows", "arm64")
+                .setTags("build")
+                .setHost("bmh-build-x64-win11-1"),
 
-            // Windows x64 (??)
-//            new Target("windows", "x64", "bmh-jjlauer-1", null),
+        // Windows arm64 (ONLY for testing)
+        new Target("windows", "arm64", "for testing")
+                .setTags("test")
+                .setHost("bmh-build-arm64-win11-1"),
 
-            // Linux x64 (ubuntu 18.04, glibc 2.27+)
-            new Target("linux", "x64", null, "amd64/ubuntu:18.04"),
+        // Linux x64 (ubuntu 18.04, glibc 2.27+)
+        new Target("linux", "x64")
+                .setTags("build", "test")
+                .setContainerImage("amd64/ubuntu:18.04"),
 
-            // Linux arm64 (ubuntu 18.04, glibc 2.27+)
-            new Target("linux", "arm64", "bmh-build-arm64-ubuntu22-1", "arm64v8/ubuntu:18.04"),
+        // Linux arm64 (ubuntu 18.04, glibc 2.27+)
+        new Target("linux", "arm64")
+                .setTags("build", "test")
+                .setHost("bmh-build-arm64-ubuntu22-1")
+                .setContainerImage("arm64v8/ubuntu:18.04"),
 
-            // Linux armhf (ubuntu 18.04, glibc 2.27+)
-            new Target("linux", "armhf", null, "arm32v7/ubuntu:18.04"),
+        // Linux armhf (ubuntu 18.04, glibc 2.27+)
+        new Target("linux", "armhf")
+                .setTags("build", "test")
+                .setContainerImage("arm32v7/ubuntu:18.04"),
 
-            // Linux MUSL x64 (alpine 3.11)
-            new Target("linux_musl", "x64", null, "amd64/alpine:3.11"),
+        // Linux MUSL x64 (alpine 3.11)
+        new Target("linux_musl", "x64")
+                .setTags("build", "test")
+                .setContainerImage("amd64/alpine:3.11"),
 
-            // Linux MUSL arm64 (alpine 3.11)
-            new Target("linux_musl", "arm64", "bmh-build-arm64-ubuntu22-1", "arm64v8/alpine:3.11"),
+        // Linux MUSL arm64 (alpine 3.11)
+        new Target("linux_musl", "arm64")
+                .setTags("build", "test")
+                .setHost("bmh-build-arm64-ubuntu22-1")
+                .setContainerImage("arm64v8/alpine:3.11"),
 
-            // MacOS x64 (10.13+)
-            new Target("macos", "x64", "bmh-build-x64-macos1013-1", null),
+        // MacOS x64 (10.13+)
+        new Target("macos", "x64")
+                .setTags("build", "test")
+                .setHost("bmh-build-x64-macos1013-1"),
 
-            // MacOS arm64 (12+)
-            new Target("macos", "arm64", "bmh-build-arm64-macos12-1", null),
+        // MacOS arm64 (12+)
+        new Target("macos", "arm64")
+                .setTags("build", "test")
+                .setHost("bmh-build-arm64-macos12-1"),
 
-            // Linux riscv64 (ubuntu 20.04, glibc 2.31+)
-            new Target("linux", "riscv64", null, "riscv64/ubuntu:20.04")
+        // Linux riscv64 (ubuntu 20.04, glibc 2.31+)
+        new Target("linux", "riscv64")
+                .setTags("build", "test")
+                .setContainerImage("riscv64/ubuntu:20.04")
 
-            // potentially others could be built too
-            // arm32v5/debian linux-armel
-            // mips64le/debian linux-mips64le
-            // s390x/debian linux-s390x
-            // ppc64le/debian linux-ppc64le
-        );
-    }
+        // potentially others could be built too
+        // arm32v5/debian linux-armel
+        // mips64le/debian linux-mips64le
+        // s390x/debian linux-s390x
+        // ppc64le/debian linux-ppc64le
+    );
 
     public void build_containers() throws Exception {
-        this.execute((target, project) -> {
-            if (project.hasContainer()) {
-                project.exec("setup/build-docker-container-action.sh", target.getBaseDockerImage(), project.getContainerName()).run();
-            }
-        });
+        new Buildx(targets)
+            .execute((target, project) -> {
+                if (project.hasContainer()) {
+                    project.exec("setup/build-docker-container-action.sh", target.getContainerImage(), project.getContainerName(), target.getOs(), target.getArch()).run();
+                }
+            });
     }
 
     public void build_native_libs() throws Exception {
-        this.execute((target, project) -> {
-            String buildScript = "setup/build-native-lib-linux-action.sh";
-            if (target.getOs().equals("windows")) {
-                buildScript = "setup/build-native-lib-windows-action.bat";
-            }
+        new Buildx(targets)
+            .setTags("build")
+            .execute((target, project) -> {
+                String buildScript = "setup/build-native-lib-linux-action.sh";
+                if (target.getOs().equals("windows")) {
+                    buildScript = "setup/build-native-lib-windows-action.bat";
+                }
 
-            project.action(buildScript, target.getOs(), target.getArch()).run();
+                project.action(buildScript, target.getOs(), target.getArch()).run();
 
-            // we know that the only modified file will be in the artifact dir
-            final String artifactRelPath = "tkrzw-" + target.getOsArch() + "/src/main/resources/jne/" + target.getOs() + "/" + target.getArch() + "/";
-            project.rsync(artifactRelPath, artifactRelPath).run();
-        });
+                // we know that the only modified file will be in the artifact dir
+                final String artifactRelPath = "tkrzw-" + target.getOsArch() + "/src/main/resources/jne/" + target.getOs() + "/" + target.getArch() + "/";
+                project.rsync(artifactRelPath, artifactRelPath).run();
+            });
     }
 
     public void tests() throws Exception {
-        this.execute((target, project) -> {
-            // we cannot test the windows-arm64 executable on our windows-x64 host that compiles it
-            // so we will need to simply skip this target on our test run
-            if (target.getOs().equals("windows") && target.getArch().equals("arm64")) {
-                throw new SkipException("cannot test on this host");
-            }
+        new Buildx(targets)
+            .setTags("test")
+            .execute((target, project) -> {
+                String testScript = "setup/test-project-action.sh";
+                if (target.getOs().equals("windows")) {
+                    testScript = "setup/test-project-action.bat";
+                }
 
-            String testScript = "setup/test-project-action.sh";
-            if (target.getOs().equals("windows")) {
-                testScript = "setup/test-project-action.bat";
-            }
-
-            project.action(testScript).run();
-        });
+                project.action(testScript).run();
+            });
     }
 
 }
